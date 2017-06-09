57c23066b249e86419a68ad20ce8083d5ba1f039
Jonah Siekmann <siekmanj@oregonstate.edu>
2017-06-08 17:18:55 -0700

    Added line drawing on canvas

--- app.js
+++ app.js
@@ -3,25 +3,27 @@ var app = express();
 var serv = require('http').Server(app);
 var port = 80;
 var io = require('socket.io')(serv, {});
+var deltaT = 1000/40;
+
+app.use('/client', express.static(__dirname + '/client'));
+
+serv.listen(port);
+console.log("Server started on port " + port);
 
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
 
-app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(port);
-
-console.log("Server started on port " + port);
-
-var deltaT = 1000/40;
 
 var SOCKET_LIST = {};
 
 io.sockets.on('connection', function(socket){
     
     socket.id = Math.floor(Math.random()*100000);
+    
+    //Hex color selection
     
 });
 
